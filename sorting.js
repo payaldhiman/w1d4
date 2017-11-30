d0cda7fb8b9e04b6565b0297e245bb32c5774f66
Payal Dhiman <payaldhiman@outlook.com>
2017-11-30 23:05:56 +0000

    using sort()

---
+++ sorting.js
@@ -0,0 +1,27 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+
+
+var result = students.sort(function(a,b){
+  var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
+    if (nameA < nameB) //sort string ascending
+        return -1
+    if (nameA > nameB)
+        return 1
+    if (nameA == nameB){
+      return b.age-a.age
+    }
+    return 0
+
+});
+
+
+
+console.log(result);
+
+
